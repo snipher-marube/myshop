8a03d24892c422d0dfccb3aa4014f95637e8ec69
Snipher Marube <sniphermarube@gmail.com>
2024-03-07 16:08:25 +0300

    Generating PDF invoices dynamically

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
 from .tasks import order_created
+from orders.models import Order
 
 def order_create(request):
     cart = Cart(request)
@@ -27,4 +33,21 @@ def order_create(request):
         'form': form,
     }
     return render(request, 'orders/order/create.html', context)
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    context = {
+        'order': order
+    }
+    return render(request, 'admin/orders/order/detail.html', context)
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
 
